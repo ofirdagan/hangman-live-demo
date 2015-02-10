1b5cb18b65b15efc0ed7cf7714247d8db3cb70fd
Ofir Dagan <ofirdagan@gmail.com>
2015-02-10 13:10:41 +0200

    game branch

--- app/scripts/services/game.js
+++ app/scripts/services/game.js
@@ -5,8 +5,27 @@
   /* @ngInject */
   function gameFactory() {
 
-    function Game() {
+    function Game(guessWord) {
+      this.guessWord = guessWord;
+      this.charactersInLanguage = 'abcdefghijklmnopqrstuvwxyz'.split('');
+      var selectedLetters = [];
+
+      this.selectLetter = function (char) {
+        selectedLetters.push(char);
+      };
+
+      this.isLetterSelected = function (char) {
+        return selectedLetters.indexOf(char) !== -1;
+      };
+
+      this.getRevealedLettersArray = function () {
+        return guessWord.split('').map(function (char) {
+          var shouldShowChar = char === ' ' || selectedLetters.indexOf(char) !== -1;
+          return shouldShowChar ? char : '_';
+        });
+      };
     }
+
     return Game;
   }
 
