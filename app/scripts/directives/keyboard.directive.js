1e45b2d49765e914740a8ac5862ac4ee098a9b27
Ofir Dagan <ofirdagan@gmail.com>
2015-02-10 13:52:10 +0200

    keyboard branch

--- app/scripts/directives/keyboard.directive.js
+++ app/scripts/directives/keyboard.directive.js
@@ -5,6 +5,18 @@
   /* @ngInject */
   function keyboard() {
     return {
+      templateUrl: 'views/keyboard.view.html',
+      restrict: 'E',
+      scope: {
+        keys: '=',
+        wasSelected: '&'
+      },
+      link: function postLink(scope) {
+        scope.onKeyClick = function (key) {
+          scope.$emit('letterSelected', key);
+        };
+        scope.wasSelected = scope.wasSelected();
+      }
     };
   }
 
