81437d3fb9692a2e2e3dc09a3f1c74b63e61a265
Ofir Dagan <ofirdagan@gmail.com>
2015-02-11 09:54:00 +0200

    after game factory

--- test/spec/services/game.spec.js
+++ test/spec/services/game.spec.js
@@ -7,8 +7,43 @@ describe('Service: game', function () {
     module('hangmanAppInternal');
   });
 
-  it('a dummy test', function () {
-    aGame('');
+  it('should have a guess word', function () {
+    var game = aGame('amsterdam');
+    expect(game.guessWord).toBe('amsterdam');
+  });
+
+  it('should have the characters in language', function () {
+    var game = aGame();
+
+    expect(game.charactersInLanguage).toEqual('abcdefghijklmnopqrstuvwxyz'.split(''));
+  });
+
+  it('should select letters', inject(function () {
+    //given
+    var game = aGame('Chazz Michael Michaels');
+
+    //when
+    game.selectLetter('a');
+
+    //then
+    expect(game.isLetterSelected('a')).toBe(true);
+    expect(game.isLetterSelected('i')).toBe(false);
+  }));
+
+  it('should return the guess word in its current status', function () {
+    var game = aGame('amsterdam');
+
+    expect(game.getRevealedLettersArray()).toEqual('_________'.split(''));
+
+    game.selectLetter('a');
+    expect(game.getRevealedLettersArray()).toEqual('a______a_'.split(''));
+
+    game.selectLetter('o');
+    expect(game.getRevealedLettersArray()).toEqual('a______a_'.split(''));
+
+    game.selectLetter('t');
+    expect(game.getRevealedLettersArray()).toEqual('a__t___a_'.split(''));
+
   });
 
   // instantiate service
