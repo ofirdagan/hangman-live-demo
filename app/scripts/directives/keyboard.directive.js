619cdfbe5ef0fdb4738a64d2f60dd2ecfdffe647
Ofir Dagan <ofirdagan@gmail.com>
2015-02-11 09:57:57 +0200

    after keyboard directive

--- app/scripts/directives/keyboard.directive.js
+++ app/scripts/directives/keyboard.directive.js
@@ -3,12 +3,27 @@
 (function () {
 
   /* @ngInject */
-  function keyboard() {
+  function keyboard(hangmanEvents) {
     return {
+      templateUrl: 'views/keyboard.view.html',
+      restrict: 'E',
+      scope: {
+        keys: '=',
+        wasSelected: '&'
+      },
+      link: function postLink(scope) {
+        scope.onKeyClick = function (key) {
+          scope.$emit(hangmanEvents.letterSelected, key);
+        };
+        scope.wasSelected = scope.wasSelected();
+      }
     };
   }
 
   angular
     .module('hangmanAppInternal')
+    .constant('hangmanEvents', {
+      letterSelected: 'letterSelected'
+    })
     .directive('keyboard', keyboard);
 })();
--- test/spec/directives/keyboard.directive.spec.js
+++ test/spec/directives/keyboard.directive.spec.js
@@ -2,7 +2,7 @@
 
 describe('Directive: keyboard', function () {
 
-  //var element;
+  var element;
   var scope;
 
   // load the directive's module
@@ -13,13 +13,56 @@ describe('Directive: keyboard', function () {
     });
   });
 
-  //
-  //function compile(template) {
-  //  inject(function ($rootScope, $compile) {
-  //    element = angular.element(template);
-  //    element = $compile(element)(scope);
-  //  });
-  //  scope.$digest();
-  //}
+  it('should hold keys', inject(function () {
+    scope.keys = ['a', 'b', 'c', 'd'];  // usually goes till z..
+
+    compile('<keyboard keys="keys"></keyboard>');
+
+    expect(element.isolateScope().keys).toBe(scope.keys);
+  }));
+
+  it('should emit letterSelected event on key click', inject(function (hangmanEvents) {
+    //given
+    scope.keys = ['a', 'b', 'c', 'd'];  // usually goes till z..
+    compile('<keyboard keys="keys"></keyboard>');
+
+    var eventSpy = jasmine.createSpy('event spy');
+    scope.$on(hangmanEvents.letterSelected, eventSpy);
+
+    //when
+    element.isolateScope().onKeyClick('a');
+
+    //then
+    expect(eventSpy).toHaveBeenCalledWith(jasmine.any(Object), 'a');
+  }));
+
+  it('should add class was-selected when wasSelected returns true', function () {
+    //given
+    scope.keys = ['a', 'b', 'c', 'd'];  // usually goes till z..
+    scope.wasSelected = function (key) {
+      return key === 'a';
+    };
+    compile('<keyboard keys="keys" was-selected="wasSelected"></keyboard>');
+
+    //when
+    scope.$digest();
+
+    //then
+    var letterA = element.find('.key')[0];
+    expect(letterA).toHaveClass('was-selected');
+
+    var letterB = element.find('.key')[1];
+    expect(letterB).not.toHaveClass('was-selected');
+
+    expect(element.find('.was-selected').length).toBe(1);
+  });
+
+  function compile(template) {
+    inject(function ($rootScope, $compile) {
+      element = angular.element(template);
+      element = $compile(element)(scope);
+    });
+    scope.$digest();
+  }
 
 });
